6fc49a0c59e8196e47f97638ab622a18d14e4db2
Nam Nguyễn <giapdong992@gmail.com>
2020-07-15 21:37:35 +0700

    Add promise for readdir and fs.stat

--- test.js
+++ test.js
@@ -1,11 +1,19 @@
 
 const disk = require('./lib/main');
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path')
+const colors = require('colors')
+const Events = require('events')
 
-disk.Scan("F:\\Project\\ATV", 100000000)
+colors.setTheme({
+    success: 'green',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
+});
+
+disk.Scan("F:\\Project\\eco4p", 10000)
+// disk.ScanAndFilter("F:\\ProgramData", 10000)
+// disk.Scan(__dirname, 10000)
 
 // disk.Compare();
-
-// let result = fs.readdirSync("F:\\Project") //Scan all item into directory with path pathX
-
-// console.log(result)
