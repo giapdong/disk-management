43fa1daa6c93027e03bceb45bea3f0831e047800
Giáp Đông <giapdong992@gmail.com>
2021-02-01 22:56:40 +0700

    Separate 4 step Scan module

--- lib/_index.ts
+++ lib/_index.ts
@@ -9,36 +9,16 @@ import * as ScanHelper from "@lib/helper/scan.helper";
 import * as FS_TOOLS from "@lib/tools/filesystem";
 
 colors.setTheme(color);
-const spinner = ora({
-  text: "[1/4] Scanning",
-  spinner: {
-    interval: 80,
-    frames: ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
-  }
-});
+
 const scanDir = path.join(__dirname, "..", "scan");
 const compareDir = path.join(__dirname, "..", "compare");
 
 export async function Scan(root = __dirname, threshold = 1000000, mode = ScanMode.Normal) {
   console.time("Disk-management-scanner");
 
-  let HierachyTree: Hierachy = new Hierachy(null, root, 0, TypeNodeHierachy.Directory);
-  spinner.start();
-  await ScanHelper.scanHierachyNode(spinner, HierachyTree);
-  spinner.succeed("[1/4] Scanning");
-
-  console.log(`Total storage: ${bytesToSize(HierachyTree.storage)}`);
-
-  spinner.text = "[2/4] Scanning big directory";
-  spinner.start();
-  let listBigNode: BigNode[] = ScanHelper.getBigDirectoryFromRootHierachy(HierachyTree, threshold);
-  console.log(`\n${listBigNode.length} directory contrain total file file size >= ${threshold}`);
-  spinner.succeed("[2/4] Scanning big directory\n");
-
-  spinner.text = "[3/4] Formatting data";
-  spinner.start();
-  ScanHelper.removeParent(HierachyTree);
-  spinner.succeed("[3/4] Formatting data");
+  let HierachyTree: Hierachy = await ScanHelper.scanInFileSystem(root);
+  let listBigNode: BigNode[] = await ScanHelper.scanBigDirectoryInHierachy(HierachyTree, threshold);
+  HierachyTree = await ScanHelper.removeParentInHierachy(HierachyTree);
 
   let pathJSON = path.join(scanDir, getDateByFormat() + ".log");
   let obj = {
@@ -48,26 +28,7 @@ export async function Scan(root = __dirname, threshold = 1000000, mode = ScanMod
     root: mode == ScanMode.Normal ? HierachyTree : listBigNode
   };
 
-  spinner.text = "[4/4] Writting result";
-  spinner.start();
-
-  try {
-    //Create scanDir
-    if (!fs.existsSync(scanDir)) fs.mkdirSync(scanDir);
-
-    // JSON.stringify(obj, null, 4)
-    await FS_TOOLS.writeFilePromise(pathJSON, JSON.stringify(obj));
-    spinner.succeed("[4/4] Writting result");
-
-    console.log("Finish" + " Saved 1 new log file.");
-    console.timeEnd("Disk-management-scanner");
-  } catch (error) {
-    spinner.fail("[4/4] Writting result");
-
-    console.log("Failed" + " Write file log return with Errror");
-    console.timeEnd("Disk-management-scanner");
-  }
+  await ScanHelper.writeResultToFile(scanDir, pathJSON, obj);
 
   console.timeEnd("Disk-management-scanner");
-  process.exit();
 }
--- lib/helper/scan.helper.ts
+++ lib/helper/scan.helper.ts
@@ -1,8 +1,100 @@
-import { Stats } from "fs";
+import fs, { Stats } from "fs";
 import * as FS_TOOLS from "@lib/tools/filesystem";
 import { Hierachy } from "@lib/bean/NodeHierachy";
 import { BigNode, StatsNode, TypeNodeHierachy } from "@root/types";
-import { Ora } from "ora";
+import ora, { Ora } from "ora";
+import { bytesToSize } from "@lib/helper/global.helper";
+
+/**
+ * Scan in filesystem
+ *
+ * @param rootPath Path pass by argument
+ */
+export async function scanInFileSystem(rootPath: string): Promise<Hierachy> {
+  const spinner = ora({
+    text: "[1/4] Scanning",
+    spinner: {
+      interval: 80,
+      frames: ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
+    }
+  });
+
+  let HierachyTree: Hierachy = new Hierachy(null, rootPath, 0, TypeNodeHierachy.Directory);
+  spinner.start();
+  await scanHierachyNode(spinner, HierachyTree);
+  spinner.info(`Total storage: ${bytesToSize(HierachyTree.storage)}`);
+  spinner.succeed("[1/4] Scanning");
+
+  return HierachyTree;
+}
+
+/**
+ * Scan big directory in Hierachy node
+ *
+ * @param rootHierachy Root Hierachy
+ * @param threshold Threshold wanna filter
+ */
+export async function scanBigDirectoryInHierachy(rootHierachy: Hierachy, threshold: number): Promise<BigNode[]> {
+  const spinner = ora({
+    text: "[2/4] Scanning big directory",
+    spinner: {
+      interval: 80,
+      frames: ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
+    }
+  });
+  spinner.start();
+  let listBigNode: BigNode[] = getBigDirectoryFromRootHierachy(rootHierachy, threshold);
+  spinner.info(`[2/4] ${listBigNode.length} directory contrain total file file size >= ${threshold}`);
+  spinner.succeed("[2/4] Scanning big directory");
+
+  return listBigNode;
+}
+
+/**
+ * Remove parent reference in Hierachy instance
+ *
+ * @param rootHierachy Root Hierachy
+ */
+export async function removeParentInHierachy(rootHierachy: Hierachy): Promise<Hierachy> {
+  const spinner = ora({
+    text: "[3/4] Formatting data",
+    spinner: {
+      interval: 80,
+      frames: ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
+    }
+  });
+
+  spinner.start();
+  removeParent(rootHierachy);
+  spinner.succeed("[3/4] Formatting data");
+  return rootHierachy;
+}
+
+export async function writeResultToFile(scanDir: string, pathJSON: string, obj: any) {
+  const spinner = ora({
+    text: "[4/4] Writting result",
+    spinner: {
+      interval: 80,
+      frames: ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
+    }
+  });
+  spinner.start();
+
+  try {
+    //Create scanDir
+    if (!fs.existsSync(scanDir)) fs.mkdirSync(scanDir);
+
+    // JSON.stringify(obj, null, 4)
+    await FS_TOOLS.writeFilePromise(pathJSON, JSON.stringify(obj));
+
+    spinner.info("Finish" + " Saved 1 new log file.");
+    spinner.succeed("[4/4] Writting result");
+  } catch (error) {
+    console.log(error);
+    spinner.info("Failed" + " Write file log return with Errror");
+    spinner.fail("[4/4] Writting result");
+  }
+}
 
 /**
  * Scan and fill to Hierachy node
