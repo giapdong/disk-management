f2a09d567a2054e80e660f37464b5ea3b21c823d
Giáp Đông <giapdong992@gmail.com>
2021-02-06 22:08:38 +0700

    Update example and package.json file

--- examples/lib.example.ts
+++ examples/lib.example.ts
@@ -1,5 +1,5 @@
-import * as lib from "../src/lib/_index";
+import * as lib from "../src/lib/index";
 import path from "path";
 
-// lib.Scan(path.join(__dirname, ".."), 1000);
-lib.Compare(10000);
+lib.Scan(path.join(__dirname, ".."), 1000);
+// lib.Compare(10000);
