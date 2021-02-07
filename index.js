7dd4d73a4cee85752c49acfe262d90324bd902ac
Giáp Đông <giapdong992@gmail.com>
2021-02-07 17:41:11 +0700

    Refix cli command

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 "use strict";
 const lib = require("./build/lib/index");
+const interfaces = require("./build/lib/interface/index");
 
-module.exports = { ...lib };
+module.exports = { ...lib, ...interfaces };
