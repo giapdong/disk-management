ef168f818f6e162f9c1ab07ce4dbbfb0e323758b
Giáp Đông <giapdong992@gmail.com>
2021-02-07 07:32:20 +0700

    Rename example file and remove command run in package.json file

--- examples/api-system-partition.js
+++
@@ -1,8 +0,0 @@
-const disk = require("../index.js");
-
-const executor = async () => {
-  let data = await disk.readSystemPartition();
-  console.log(data);
-  console.log(disk.bytesToSize(20241813504));
-};
-executor();
--- examples/lib.ts
+++ examples/lib.ts
@@ -1,3 +1,6 @@
+// Run command: yarn ts-node-dev lib-example.ts
+// Run command: npx ts-node-dev lib-example.ts
+
 import * as lib from "../src/lib/index";
 import path from "path";
 
